65190977459a6f7b8ad217f20a590fd4ecfb1c4e
aru <arukaszhen@gmail.com>
2026-04-12 20:53:41 +0500

    front routings

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,33 +1,11 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { ApiService } from './services/api.service';
-import { CommonModule } from '@angular/common';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
-  providers: [ApiService],
 })
-export class App implements OnInit {
-  protected readonly title = signal('DoMePls');
-  protected message = signal<string>('Loading...');
-  protected error = signal<string | null>(null);
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit() {
-    this.apiService.getTestMessage().subscribe({
-      next: (response) => {
-        this.message.set(response.message);
-      },
-      error: (err) => {
-        this.error.set('Failed to load message from backend');
-        console.error(err);
-      },
-    });
-  }
-}
+export class App {}
