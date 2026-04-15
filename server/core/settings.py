058c2991fd0bc3eaaa089cea69651ac9b8bc2bf0
aablty <acrouple@yandex.ru>
2026-04-16 04:45:31 +0500

    feat(logging): add logging configuration for Django and Gunicorn

--- server/core/settings.py
+++ server/core/settings.py
@@ -197,3 +197,34 @@ CLOUDINARY_STORAGE = {
 }
 
 MEDIA_URL = '/media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'gunicorn.error': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'gunicorn.access': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
