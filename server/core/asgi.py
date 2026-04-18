f79d455d20af8bde46a715224c01908647b8051f
aablty <acrouple@yandex.ru>
2026-04-18 11:17:40 +0500

    style: reorder import statements in asgi.py for improved clarity

--- server/core/asgi.py
+++ server/core/asgi.py
@@ -1,4 +1,3 @@
-from core.routing import application as websocket_application
 import os
 
 from channels.routing import ProtocolTypeRouter
@@ -6,8 +5,9 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-
 django_asgi_application = get_asgi_application()
+
+from core.routing import application as websocket_application
 
 application = ProtocolTypeRouter(
     {
